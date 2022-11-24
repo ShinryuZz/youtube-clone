bf56af37a5bd649694cc60021e83847c4dce13d2
Shinryuzz <@@>
2022-11-24 09:25:54 +0900

    fix

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,22 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { MantineProvider } from '@mantine/core'
-import Head from "next/head"
-import { NotificationsProvider } from '@mantine/notifications'
-import { NextPage } from 'next'
-import { ReactElement, ReactNode } from 'react'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
+import { NotificationsProvider } from "@mantine/notifications";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 type NextPageWithLayout = NextPage & {
-  getLayout? : (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page)=>page);
-
+  const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <>
@@ -31,15 +33,19 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         withGlobalStyles
         withNormalizeCSS
         theme={{
-            colorScheme: "light",
+          colorScheme: "light",
         }}
       >
         <NotificationsProvider>
-          {getLayout(<main>
-            <Component {...pageProps} />
-          </main>)}
+          <QueryClientProvider client={queryClient}>
+            {getLayout(
+              <main>
+                <Component {...pageProps} />
+              </main>
+            )}
+          </QueryClientProvider>
         </NotificationsProvider>
       </MantineProvider>
     </>
-    )
+  );
 }
--- client/pages/auth/register.tsx
+++ client/pages/auth/register.tsx
@@ -1,5 +1,105 @@
+import {
+  Button,
+  Container,
+  Paper,
+  Stack,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/hooks";
+import { showNotification } from "@mantine/notifications";
+import { AxiosError } from "axios";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useMutation } from "react-query";
+import { registerUser } from "../api";
+
 function RegisterPage() {
-  return <div>register page</div>;
+  const router = useRouter;
+
+  const form = useForm({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const mutation = useMutation<
+    string,
+    AxiosError,
+    Parameters<typeof registerUser>["0"]
+  >(registerUser, {
+    onMutate: () => {
+      showNotification({
+        id: "register",
+        title: "Creating account",
+        message: "Please wait...",
+        loading: true,
+      });
+    },
+    onSuccess: () => {
+      showNotification({
+        id: "register",
+        title: "Success",
+        message: "Sucecssfully created account",
+        loading: true,
+      });
+
+      router.push("/auth/login");
+    },
+    onError: () => {
+      showNotification({
+        id: "register",
+        title: "Error",
+        message: "Could not create account",
+        loading: true,
+      });
+    },
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Register User </title>
+        <Container>
+          <Title>Register</Title>
+          <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+            <form onSubmit={form.onSubmit((values) => mutation.mutate(values))}>
+              <Stack>
+                <TextInput
+                  label="Email"
+                  placeholder="jane@example.com"
+                  required
+                  {...form.getInputProps("email")}
+                />
+                <TextInput
+                  label="Username"
+                  placeholder="shinryuzz"
+                  required
+                  {...form.getInputProps("username")}
+                />
+                <TextInput
+                  label="Password"
+                  required
+                  placeholder="Your strong password"
+                  {...form.getInputProps("password")}
+                />
+                <TextInput
+                  label="Confirm Password"
+                  placeholder="Your strong password"
+                  required
+                  {...form.getInputProps("confirmPassword")}
+                />
+                <Button type="submit">Register</Button>
+              </Stack>
+            </form>
+          </Paper>
+        </Container>
+      </Head>
+    </>
+  );
 }
 
 export default RegisterPage;
